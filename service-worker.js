eaaf66d6f129c23c0170b8304508689b64e99486
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-05 02:55:34 +0000

    Deploy webpack.lipengzhou.com to github.com/lipengzhou/webpack-tutorial.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -21,15 +21,19 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "6facf9ef9627294b789c4ec3eba1c934"
+    "revision": "6fc612e8a41fb80ea0bbdcb617607730"
   },
   {
     "url": "advanced-concept.html",
-    "revision": "1ea11a87d4b4ea6b6cb120fd9fd2b005"
+    "revision": "b9c6852afd30be299134e29fea4d479f"
   },
   {
     "url": "assets/css/0.styles.f9563ee1.css",
     "revision": "78b5c8e85f0f7be11de082209c821c25"
+  },
+  {
+    "url": "assets/img/1554342113665.ee3d47b5.png",
+    "revision": "ee3d47b5e9bdd8208b6a0514a1bd71bf"
   },
   {
     "url": "assets/img/logo-on-white-bg.81da10d7.png",
@@ -44,92 +48,92 @@ self.__precacheManifest = [
     "revision": "f78661bef717cf2cc2c2e5158f196384"
   },
   {
-    "url": "assets/js/10.25bd5c73.js",
-    "revision": "46cd7a2173cec40b3cd70c9818fa5faa"
+    "url": "assets/js/10.b124fc1d.js",
+    "revision": "d1aef0e182e3dffc61260f9a5e193a62"
   },
   {
-    "url": "assets/js/11.c881af84.js",
-    "revision": "c484ccef782c5320a4ff3463fdbf2fc9"
+    "url": "assets/js/11.33442fcb.js",
+    "revision": "4fe1dbae9d0b79869252d1c5b81a6fa1"
   },
   {
-    "url": "assets/js/12.a1dc1144.js",
-    "revision": "869ce052dc06b9f27898f8cdcf6a0c41"
+    "url": "assets/js/12.8c1025e1.js",
+    "revision": "e23df42d305358d569bc369627f3819c"
   },
   {
-    "url": "assets/js/13.d4fb21e8.js",
-    "revision": "0912a238a64a8a32ed6cbe99458f3d0c"
+    "url": "assets/js/13.efcab1f6.js",
+    "revision": "493209a31c990b3a6cebb199e98f95bb"
   },
   {
-    "url": "assets/js/14.d553a3d6.js",
-    "revision": "7445d24d56c6de39ea474f5e2a13403a"
+    "url": "assets/js/14.28835ea1.js",
+    "revision": "410e39b68034a2d659922c7911ba48f7"
   },
   {
-    "url": "assets/js/15.1b42c259.js",
-    "revision": "cd15a96918a36703ec4721b3b684c98d"
+    "url": "assets/js/15.50f7a17f.js",
+    "revision": "1276cc5b32a11c7410c8cc8c6454e61c"
   },
   {
-    "url": "assets/js/16.36a50a94.js",
-    "revision": "67e821efacb3cfee02a9c86545cf1387"
+    "url": "assets/js/16.eab670f8.js",
+    "revision": "fdb46f0398c985267f9f7c6088cc4cf6"
   },
   {
-    "url": "assets/js/17.0bb96ce8.js",
-    "revision": "660bdc4e8a8362f8936b156664adb052"
+    "url": "assets/js/17.a1bdef0e.js",
+    "revision": "36c30bdf5ab4487d08899384e0fc3c72"
   },
   {
     "url": "assets/js/18.c93f17da.js",
     "revision": "f61180964c2375236968274a66acddfa"
   },
   {
-    "url": "assets/js/2.fe8686b1.js",
-    "revision": "873ae58edc8bb2daf4db853bfad65c0f"
+    "url": "assets/js/2.5ebdbdf5.js",
+    "revision": "a7b8d7cc9cce21eab3fa1ece0045561b"
   },
   {
-    "url": "assets/js/3.6fb41313.js",
-    "revision": "e643d67f56515fd32062d6771dfdea5a"
+    "url": "assets/js/3.32fa2c79.js",
+    "revision": "3e2595da56a01e5ab37db14f6f059f24"
   },
   {
-    "url": "assets/js/4.877b33ce.js",
-    "revision": "290e77b209b067798cce39089c69043d"
+    "url": "assets/js/4.6b3b0a31.js",
+    "revision": "1edac9fba9e8f5ebfc54a86018723c00"
   },
   {
-    "url": "assets/js/5.bb9d4fa5.js",
-    "revision": "d2407d08250c6348d7adcb7ff1639f9c"
+    "url": "assets/js/5.550a6898.js",
+    "revision": "fdcda1a994218cc572a7c0178df22096"
   },
   {
-    "url": "assets/js/6.47e3d73f.js",
-    "revision": "f4a3238ba4b5af26cbf1981e64d0052d"
+    "url": "assets/js/6.92fb2d42.js",
+    "revision": "fe9d39d60af7951bb28bda330e0f0898"
   },
   {
-    "url": "assets/js/7.37d34e7a.js",
-    "revision": "913989e34c8795953ccc0310c6864048"
+    "url": "assets/js/7.d5582872.js",
+    "revision": "24c45f3117c184c53c99a427d17e5695"
   },
   {
-    "url": "assets/js/8.1effbb51.js",
-    "revision": "13f40f4144864adb482cab85a5d104e1"
+    "url": "assets/js/8.4c9a2050.js",
+    "revision": "36e063c063f8e0907cd23f0608706921"
   },
   {
-    "url": "assets/js/9.ea7acb49.js",
-    "revision": "9da130e64d4a4479caada9a1eea70953"
+    "url": "assets/js/9.4a5721bf.js",
+    "revision": "c1506273871d1aadb6b8be391f785a78"
   },
   {
-    "url": "assets/js/app.ba08c4c0.js",
-    "revision": "ccdad9b7907813dc1d9b1070f6dffc3c"
+    "url": "assets/js/app.ac76fc45.js",
+    "revision": "6da6b1eeff950f7a6d6b671ca1213e7e"
   },
   {
     "url": "build-other-assets.html",
-    "revision": "19d5eb7e1ebd62b60124708c6e6b5c29"
+    "revision": "fdedb4b414c8bb38d85129bac282548e"
   },
   {
     "url": "build-performance.html",
-    "revision": "69e021fdde95a32eafed9cfd3d378db4"
+    "revision": "b40dc7dd4ddcbd4b530429aa92fa8434"
   },
   {
     "url": "config-development.html",
-    "revision": "4ea749cb9df922af173dcdb702500d7e"
+    "revision": "416326a9cce15f2cf2d3201094f35a69"
   },
   {
     "url": "getting-started.html",
-    "revision": "bb916358a7675152b4fa6e20dd7a4576"
+    "revision": "a5cffd18bd767e286ca2131aa343a1e4"
   },
   {
     "url": "icons/android-chrome-128x128.png",
@@ -141,11 +145,11 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "65ae9ca5e97cbcf242c85fd9ac8ae647"
+    "revision": "b83aa1e1e9e0fbe3094d5ac71b1fa4c5"
   },
   {
     "url": "introduction.html",
-    "revision": "fe858d53d3923c8aee69613e9a8e6b74"
+    "revision": "e9c7dba9feb93b359d9738b4b082c8f1"
   },
   {
     "url": "logo.png",
@@ -153,23 +157,23 @@ self.__precacheManifest = [
   },
   {
     "url": "webpack-angular.html",
-    "revision": "b5629f0acfa1a599a55667ccf9db0209"
+    "revision": "d64663f10203f45e53cf792c5c45f236"
   },
   {
     "url": "webpack-build-performance.html",
-    "revision": "1ba1f720265e228369ac4c2d432c2f4d"
+    "revision": "c7538a87543a69e483bfe9cd1639ca10"
   },
   {
     "url": "webpack-principle.html",
-    "revision": "5f243882d432ee14e854d9b0bfd8c896"
+    "revision": "28358e1f47bb2d203679c8efc14ddae7"
   },
   {
     "url": "webpack-react.html",
-    "revision": "9f036cb57591b74c32d8c607d1b2f18f"
+    "revision": "0dbdfc492bc51539bcb406e9fab25fa9"
   },
   {
     "url": "webpack-vue.html",
-    "revision": "1546e3b268b9db061297a37bd8f1b525"
+    "revision": "eedf0fa5fc3ece24164c64cea9d5f354"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
